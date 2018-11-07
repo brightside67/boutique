c2a62fc5dc181dc12869feaaa0c555a652970be6
brightside67 <mobrika@list.ru>
2018-11-07 12:16:50 +0300

    initial commit

--- webpack.config.js
+++ webpack.config.js
@@ -1,23 +1,23 @@
-var ExtractTextPlugin = require('extract-text-webpack-plugin')
-var webpack = require('webpack')
-var postcssWillChange = require('postcss-will-change')
-var postcssAssets = require('postcss-assets')
-var autoprefixer = require('autoprefixer')
-var postcssPxtorem = require('postcss-pxtorem')
-var CopyWebpackPlugin = require('copy-webpack-plugin');
-var HashOutput = require('chunkhash-replace-webpack-plugin');
-var path = require('path')
+let ExtractTextPlugin = require('extract-text-webpack-plugin');
+let webpack = require('webpack');
+let postcssWillChange = require('postcss-will-change');
+let postcssAssets = require('postcss-assets');
+let autoprefixer = require('autoprefixer');
+let postcssPxtorem = require('postcss-pxtorem');
+let CopyWebpackPlugin = require('copy-webpack-plugin');
+let HashOutput = require('chunkhash-replace-webpack-plugin');
+let path = require('path');
 
 module.exports = {
   cache: true,
   devtool: 'source-map',
   entry: {
-    bundle: ['./app/main.js', './app/main.sass']
+    bundle: ['./app/main.js', './app/main.sass'],
   },
   output: {
     path: path.resolve(__dirname, 'app/static'),
     filename: '[name].js',
-    pathinfo: false
+    pathinfo: false,
   },
   resolve: {
     mainFiles: ['index'],
@@ -26,42 +26,42 @@ module.exports = {
       component: path.resolve(__dirname, 'app/component'),
       lib: path.resolve(__dirname, 'app/lib'),
       asset: path.resolve(__dirname, 'app/asset'),
-    }
+    },
   },
   module: {
     rules: [
-       {
+      {
         test: /\.js$/,
         exclude: /node_modules/,
         use: [
           {
-            loader: 'babel-loader'
-          }
-        ]
+            loader: 'babel-loader',
+          },
+        ],
       },
       {
         test: /\.jext/,
         use: [
           {
-            loader: 'jext-loader'
-          }
-        ]
+            loader: 'jext-loader',
+          },
+        ],
       },
       {
         test: /\.(png|jpe?g|gif|svg)$/,
         use: [
           {
-            loader: 'url-loader?limit=4096&name=static/img/[name].[ext]'
-          }
-        ]
+            loader: 'url-loader?limit=4096&name=static/img/[name].[ext]',
+          },
+        ],
       },
       {
         test: /\.(eot|svg|ttf|woff|woff2)$/,
         use: [
           {
-            loader: 'file-loader?name=asset/fonts/[name].[ext]'
-          }
-        ]
+            loader: 'file-loader?name=asset/fonts/[name].[ext]',
+          },
+        ],
       },
       {
         test: /\.(s?css|sass)/,
@@ -69,34 +69,36 @@ module.exports = {
           fallback: 'style-loader',
           use: [
             {
-              loader: 'css-loader'
-              // options: {
-              //   sourceMap: true
-              // }
+              loader: 'css-loader',
+              options: {
+                minimize: true,
+              },
             }, {
               loader: 'postcss-loader',
               options: {
                 plugins: [
                   postcssWillChange(),
-                  postcssAssets({basePath: './app/static/img'}),
-                  autoprefixer({browsers: [
-                    'last 2 versions',
-                    'IE >= 9',
-                    'opera 12',
-                    'safari 7',
-                    'Android >= 4',
-                    'iOS >= 7'
-                  ]}),
-                  postcssPxtorem()
-                ]
-              }
+                  postcssAssets({ basePath: './app/static/img' }),
+                  autoprefixer({
+                    browsers: [
+                      'last 2 versions',
+                      'IE >= 9',
+                      'opera 12',
+                      'safari 7',
+                      'Android >= 4',
+                      'iOS >= 7',
+                    ],
+                  }),
+                  postcssPxtorem(),
+                ],
+              },
             }, {
-              loader: 'sass-loader'
-            }
-          ]
-        })
-      }
-    ]
+              loader: 'sass-loader',
+            },
+          ],
+        }),
+      },
+    ],
   },
   plugins: [
     // new HashOutput({
@@ -108,21 +110,21 @@ module.exports = {
     // ]),
     new ExtractTextPlugin({
       filename: '[name].css',
-      allChunks: true
+      allChunks: true,
     }),
     new webpack.optimize.UglifyJsPlugin({
       minimize: true,
       sourceMap: true,
       output: {
-        comments: false
+        comments: false,
       },
       compress: {
-        warnings: false
-      }
-    })
+        warnings: false,
+      },
+    }),
   ],
   watchOptions: {
     poll: 1000,
-    aggregateTimeout: 500
-  }
-}
+    aggregateTimeout: 500,
+  },
+};
