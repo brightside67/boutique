a810072aac8dec73638d199346b1852a31c966a8
brightside67 <mobrika@list.ru>
2018-12-22 19:25:55 +0300

    fix old js snipper

--- app/main.js
+++ app/main.js
@@ -1,12 +1,1 @@
-let doc = window.document;
-
-
-if (doc.addEventListener) {
-  doc.addEventListener('DOMContentLoaded', function() {
-    console.log('DOM complete');
-
-  });
-
-} else {
-  alert('Пожалуйста, обновите ваш браузер для полноценной работы сайта. Please, renew your web browser for better performance.')
-}
+console.log('DOM is ready')
